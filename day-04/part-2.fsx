ffe477c3b54fc1c19e25b2c080c525c0d51b2360
Artur Grunau <artur.grunau@tum.de>
2015-12-06 13:00:03 +0000

    Add solutions for the problem of day 4
    Included are solutions to part 1 & 2 of the "The Ideal Stocking Stuffer"
    problem published on day 4.

---
+++ day-04/part-2.fsx
@@ -0,0 +1,25 @@
+open System.IO
+open System.Security.Cryptography
+open System.Text
+
+let computeHash (md5 : MD5) key number =
+    let input = sprintf "%s%d" key number
+
+    Encoding.UTF8.GetBytes input
+    |> md5.ComputeHash 
+
+let isAdventCoinHash (hash : byte array) =
+    hash.[0] = 0uy && hash.[1] = 0uy && hash.[2] = 0uy
+
+let mineAdventCoins md5 key =
+    Seq.initInfinite id
+    |> Seq.map (fun number -> number, computeHash md5 key number)
+    |> Seq.filter (snd >> isAdventCoinHash)
+    |> Seq.map fst
+
+let md5 = MD5.Create ()
+
+File.ReadAllText "input"
+|> mineAdventCoins md5
+|> Seq.head
+|> printfn "%d"
